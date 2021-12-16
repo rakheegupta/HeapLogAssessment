ca116b7ccccf409e9acad2fcb849cdd174e27761
Rakhee <rakheegupta@users.noreply.github.com>
2021-12-16 15:12:41 -0800

    test the library

--- app/src/main/java/com/example/sampleassessment/MainActivity.java
+++ app/src/main/java/com/example/sampleassessment/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.samplelibrary.LogUtil;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogUtil.d("Activity created");
     }
 }
