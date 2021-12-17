083adbc9049b9606a1f2e0b3d014661e87783cb7
Rakhee <rakheegupta@users.noreply.github.com>
2021-12-16 18:46:58 -0800

    add  date to library

--- SampleLibrary/src/main/java/com/example/samplelibrary/LogUtil.java
+++ SampleLibrary/src/main/java/com/example/samplelibrary/LogUtil.java
@@ -2,11 +2,17 @@ package com.example.samplelibrary;
 
 import android.util.Log;
 
+import java.util.Date;
+
 public class LogUtil {
 
     static String TAG = "SampleLibrary";
 
     public static void d(String message) {
         Log.d(TAG,message);
+    }
+
+    public static void d(String message, Date date) {
+        Log.d(TAG,message+"-"+date.toString());
     }
 }
--- app/src/main/java/com/example/sampleassessment/MainActivity.java
+++ app/src/main/java/com/example/sampleassessment/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.sampleassessment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
 
 import com.example.samplelibrary.LogUtil;
+
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ///RecyclerView scItems = findViewById(R.id.rcvItems);
+
+
+        Button btnOne = findViewById(R.id.btnOne);
+        EditText etOne = findViewById(R.id.etOne);
+
+
+        btnOne.setOnClickListener(this::onClick);
+        etOne.setOnClickListener(this::onClick);
 
         LogUtil.d("Activity created");
+    }
+    void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnOne: LogUtil.d("button one clicked at ",new Date());
+                                break;
+            case R.id.etOne: LogUtil.d("edit text one edited at ",new Date());
+                                break;
+            default: break;
+        }
     }
 }
